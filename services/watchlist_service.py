169672dc81b8c215d1d6c1853b1e68d3851ced3d
Sneha <lsneha991@gmail.com>
2026-07-13 19:52:11 -0400

    feat: add remove_from_watchlist

--- services/watchlist_service.py
+++ services/watchlist_service.py
@@ -14,6 +14,11 @@ class AlreadyInWatchlistError(Exception):
     pass
 
 
+class NotInWatchlistError(Exception):
+    """Raised when trying to remove a film that is not on the user's watchlist."""
+    pass
+
+
 def add_to_watchlist(user_id, film_id):
     """
     Save a film to a user's watchlist.
@@ -45,6 +50,33 @@ def add_to_watchlist(user_id, film_id):
     db.session.add(entry)
     db.session.commit()
     return entry
+
+
+def remove_from_watchlist(user_id, film_id):
+    """
+    Remove a film from a user's watchlist.
+
+    Args:
+        user_id (str): UUID of the user.
+        film_id (int): ID of the film.
+
+    Returns:
+        bool: True if the entry was removed.
+
+    Raises:
+        NotInWatchlistError: If the film is not on the user's watchlist.
+    """
+    entry = WatchlistEntry.query.filter_by(
+        user_id=user_id, film_id=film_id
+    ).first()
+    if entry is None:
+        raise NotInWatchlistError(
+            f"Film '{film_id}' is not on this user's watchlist"
+        )
+
+    db.session.delete(entry)
+    db.session.commit()
+    return True
 
 
 def get_watchlist(user_id):
